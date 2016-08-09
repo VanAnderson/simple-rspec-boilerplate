69819ecde722a5619fab1161fa9334c7dcf848d8
Gregory Haskins <greg@greghaskins.com>
2016-08-09 19:06:24 -0400

    Add simple failing spec

---
+++ spec/sample_spec.rb
@@ -0,0 +1,13 @@
+# Code
+panda = 'sad'
+
+# ------------------------------------------------------------------------------
+
+# Specs
+RSpec.describe 'the panda' do
+
+  it 'should be happy' do
+    expect(panda).to eq('happy')
+  end
+
+end
